148323cbcb1e4bbf9d2407675c240b8b130fb50b
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2023-04-05 23:15:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KooliNimed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kooliNimed = new Dictionary<int, string>();
+
+            kooliNimed.Add(1, "Tallinna Tööstuhariduskeskus");
+            kooliNimed.Add(2, "Tallinna Tehnikagümnaasium");
+            kooliNimed.Add(3, "TalTech");
+
+            foreach (KeyValuePair<int, string> kvp in kooliNimed)
+            {
+                Console.WriteLine("Kooli number: {0}, Kooli nimi: {1}", kvp.Key, kvp.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
